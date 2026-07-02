a43970ba4b269b5971eca99f5b65cfc640866a89
MrGabee <58374278+MrGabee@users.noreply.github.com>
2026-07-02 12:09:45 +0200

    Update bkk_monitor.py

--- bkk_monitor.py
+++ bkk_monitor.py
@@ -25,7 +25,7 @@ def magyar_ido():
 
 EMAIL_KULDO   = os.environ["EMAIL_KULDO"]
 EMAIL_JELSZO  = os.environ["EMAIL_JELSZO"]
-EMAIL_CIMZETT = os.environ["EMAIL_CIMZETT"]
+EMAIL_CIMZETT = os.environ["EMAIL_CIMZETT_BKK"]
 EMAIL_CIMZETT_KERULET = os.environ.get("EMAIL_CIMZETT_KERULET", "")
 
 # Figyelt vonalak – dél-pesti kerületek (IX., X., XIX., XVIII., XX., XXI., XXIII.)
