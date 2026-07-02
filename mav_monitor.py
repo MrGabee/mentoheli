61c33c9523f5fe0f4df7bbbeec6ad724b32aa2c4
MrGabee <58374278+MrGabee@users.noreply.github.com>
2026-07-02 08:33:11 +0200

    Fix formatting in BALESET_KIZARO list

--- mav_monitor.py
+++ mav_monitor.py
@@ -42,7 +42,7 @@ BALESET_KULCSSZAVAK = [
 BALESET_KIZARO = [
     "baleset-megelőzés", "balesetmegelőzés", "balesetmentes",
     "baleset-megelőzési", "baleset nélkül", "balesetmentesen",
-    "kerékpáros biztonság", "közlekedésbiztonsági", "meghibásodott", "műszaki ok",
+    "kerékpáros biztonság", "közlekedésbiztonsági", "meghibásodott",
 ]
 
 # meghibásodott NINCS itt – csak baleset/forgalmi ok miatti pótlóbusz kell
